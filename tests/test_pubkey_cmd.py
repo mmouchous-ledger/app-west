0eee11b008762d5771cf9d90d3e826eaf5809498
Michael Mouchous <michael.mouchous@ledger.fr>
2023-02-19 17:50:27 +0100

    Add test with wrong bip32 path

--- tests/test_pubkey_cmd.py
+++ tests/test_pubkey_cmd.py
@@ -10,3 +10,15 @@ def test_get_public_key(cmd, button, model):
     assert len(pub_key) == 32
     assert len(address) == 35
 
+def test_get_public_key_wrongbip32(cmd, button, model):
+    pub_key, address = cmd.get_public_key(
+        bip32_path="44'/5741564'/0'/0'/1'",
+        network_byte='V',
+        display=True,
+        button=button,
+        model=model
+    )  # type: bytes, bytes
+
+    assert len(pub_key) == 32
+    assert len(address) == 35
+
